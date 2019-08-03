045b7d828aeb070c5e03a3b2b06afbe4fb2d2b3a
carrotflakes <carrotflakes@gmail.com>
2019-08-03 13:20:01 +0900

    output image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate image;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+
+    let img_path = "./image.png";
+
+    let img: image::RgbImage = image::ImageBuffer::new(512, 512);
+
+    let mut img = image::ImageBuffer::from_fn(512, 512, |x, y| {
+        if x % 2 == 0 {
+            image::Rgb([0u8, 0u8, 0u8])
+        } else {
+            image::Rgb([255u8, 255u8, 255u8])
+        }
+    });
+
+    img.save(img_path).unwrap();
+
+    println!("done!");
 }
