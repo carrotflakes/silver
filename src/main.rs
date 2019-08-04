2bc48fa420c08b54ff2249cedeeffdff6af20a6e
carrotflakes <carrotflakes@gmail.com>
2019-08-04 21:27:32 +0900

    multiple objects

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,47 @@ use objects::sphere::Sphere;
 //use std::env;
 
 fn make_fn() -> Box<Fn(&Ray) -> Vec3> {
-    let sphere_center = Vec3::new(0.0, 0.0, -2.0);
-    let sphere = Sphere::new(
-        sphere_center,
-        1.0,
-        materials::PlainMat::new(Vec3::new(1.0, 0.0, 0.0)));
+    let spheres = vec![
+        Sphere::new(
+            Vec3::new(0.0, 0.0, -2.0),
+            1.0,
+            materials::PlainMat::new(Vec3::new(1.0, 0.0, 0.0))),
+        Sphere::new(
+            Vec3::new(2.0, 0.0, -2.0),
+            0.5,
+            materials::PlainMat::new(Vec3::new(1.0, 0.0, 0.0))),
+        Sphere::new(
+            Vec3::new(0.0, 0.0, -1.0),
+            0.2,
+            materials::PlainMat::new(Vec3::new(1.0, 0.0, 0.0))),
+        Sphere::new(
+            Vec3::new(0.0, 2.0, -2.0),
+            0.5,
+            materials::PlainMat::new(Vec3::new(1.0, 0.0, 0.0)))
+    ];
 
     Box::new(move |r: &Ray| -> Vec3 {
-        let t: f64 = sphere.hit(r);
-        if t > 0.0 {
-            let n = (r.at(t) - sphere_center).unit_vector();
-            return (n + Vec3::new(1.0, 1.0, 1.0)) * 0.5;
-            //return sphere.material.color();
+        let mut hit: Option<&Sphere> = Option::None;
+        let mut hit_dist = std::f64::MAX;
+        for shape in &spheres {
+            let dist: f64 = shape.hit(r);
+            if dist > 0.0 && dist < hit_dist {
+                hit = Option::Some(&shape);
+                hit_dist = dist;
+            }
         }
-        let unit_direction: Vec3 = r.direction.unit_vector();
-        let t: f64 = 0.5 * (1.0 - unit_direction.y());
-        (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+        match hit {
+            Some(shape) => {
+                let n = (r.at(hit_dist) - shape.center).unit_vector();
+                (n + Vec3::new(1.0, 1.0, 1.0)) * 0.5
+                //return sphere.material.color();
+            }
+            None => {
+                let unit_direction: Vec3 = r.direction.unit_vector();
+                let t: f64 = 0.5 * (1.0 - unit_direction.y());
+                (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+            }
+        }
     })
 }
 
