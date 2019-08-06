a33057223661aab77bc8b1b40d7b0b65315172d3
carrotflakes <carrotflakes@gmail.com>
2019-08-06 23:50:53 +0900

    allow Metal fuzz

--- src/materials/mod.rs
+++ src/materials/mod.rs
@@ -7,23 +7,24 @@ pub trait Material {
 }
 
 pub struct Metal {
-    color: Vec3,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(color: Vec3) -> Metal {
-        Metal {color: color}
+    pub fn new(fuzz: f64) -> Metal {
+        Metal {fuzz: fuzz}
     }
 }
 
 impl Material for Metal {
     fn ray(&self, ray: &Ray, location: &Vec3, normal: &Vec3) -> Ray {
         let b: Vec3 = -(ray.direction.dot(normal)) * *normal;
-        Ray::new(*location, ray.direction + 2.0 * b)
+        let f: Vec3 = self.fuzz * Vec3::random_in_unit_sphere();
+        Ray::new(*location, ray.direction + 2.0 * b + f)
     }
 
     fn color(&self, color: &Vec3) -> Vec3 {
-        *color * 0.8
+        *color
     }
 }
 
