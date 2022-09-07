5c057fed6b7355b898fcd1e9c0d8d4044ebd76ba
carrotflakes <carrotflakes@gmail.com>
2022-09-07 18:41:23 +0900

    BBox::hit()

--- src/bbox.rs
+++ src/bbox.rs
@@ -1,5 +1,6 @@
 use crate::{ray::Ray, vec3::Vec3};
 
+#[derive(Clone)]
 pub struct BBox {
     min: Vec3,
     max: Vec3,
@@ -24,5 +25,67 @@ impl BBox {
         } else {
             self.min.z() < ray.origin.z()
         })
+        // It seems there is no difference in speed even if below exists.
+        // && self.hit(ray)
     }
+
+    pub fn hit(&self, ray: &Ray) -> bool {
+        hit_xy_plane(
+            ray,
+            [self.min.x(), self.max.x()],
+            [self.min.y(), self.max.y()],
+            if 0.0 <= ray.direction.z() {
+                self.min.z()
+            } else {
+                self.max.z()
+            },
+        ) || hit_xz_plane(
+            ray,
+            [self.min.x(), self.max.x()],
+            [self.min.z(), self.max.z()],
+            if 0.0 <= ray.direction.y() {
+                self.min.y()
+            } else {
+                self.max.y()
+            },
+        ) || hit_yz_plane(
+            ray,
+            [self.min.y(), self.max.y()],
+            [self.min.z(), self.max.z()],
+            if 0.0 <= ray.direction.x() {
+                self.min.x()
+            } else {
+                self.max.x()
+            },
+        )
+    }
+}
+
+const EPS: f64 = 1e-8;
+
+fn hit_xy_plane(ray: &Ray, x: [f64; 2], y: [f64; 2], z: f64) -> bool {
+    if ray.direction.z().abs() < EPS {
+        return false;
+    }
+    let t = (z - ray.origin.z()) / ray.direction.z();
+    let hit = ray.at(t);
+    x[0] <= hit.x() && hit.x() <= x[1] && y[0] <= hit.y() && hit.y() <= y[1]
+}
+
+fn hit_xz_plane(ray: &Ray, x: [f64; 2], z: [f64; 2], y: f64) -> bool {
+    if ray.direction.y().abs() < EPS {
+        return false;
+    }
+    let t = (y - ray.origin.y()) / ray.direction.y();
+    let hit = ray.at(t);
+    x[0] <= hit.x() && hit.x() <= x[1] && z[0] <= hit.z() && hit.z() <= z[1]
+}
+
+fn hit_yz_plane(ray: &Ray, y: [f64; 2], z: [f64; 2], x: f64) -> bool {
+    if ray.direction.x().abs() < EPS {
+        return false;
+    }
+    let t = (x - ray.origin.x()) / ray.direction.x();
+    let hit = ray.at(t);
+    y[0] <= hit.y() && hit.y() <= y[1] && z[0] <= hit.z() && hit.z() <= z[1]
 }
